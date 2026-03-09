4a8508c0b90f4df22b4bd7072aaf06410a0373ca
Borges <borgespaulo72@yahoo.com>
2026-03-09 13:03:20 -0300

    fix: mudando verbatimModuleSyntax pra false

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -7,6 +7,91 @@ import { User } from "../entity/User";
 // Criar aplicação Express
 const router = express.Router();
 
+// Criar a rota para listar usuário
+router.get("/users", async (req: Request, res: Response) => {
+  try {
+    // Criar uma Instancia do repositório de User
+    const userRepository = AppDataSource.getRepository(User);
+    // Recuperar todos os usuários de banco de dados
+    const users = await userRepository.find();
+    // Retorna os usuários como resposta
+    res.status(200).json(users);
+    return;
+  } catch (error) {
+    // Retorna erro em caso de FALHA
+    res.status(500).json({
+      message: "Erro as listar os usuário!",
+    });
+    return;
+  }
+});
+// Criar a rota para visualizar um  usuário
+router.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    // Obter o ID do usuário
+    const { id } = req.params;
+
+    // Obter o repositório da entidade User
+    const userRepository = AppDataSource.getRepository(User);
+    // Buscar o usuario no banco de dados pelo ID
+    const user = await userRepository.findOneBy({ id: parseInt(id) });
+    // Verificar se o usuário fo encontrado
+    if (!user) {
+      res.status(404).json({
+        message: "Usuario não encontrada!",
+      });
+      return;
+    }
+    // retornar o usuario encontrado
+    res.status(200).json({
+      user,
+    });
+    return;
+  } catch (error) {
+    // Retornar em caso de falha
+    res.status(500).json({
+      message: "erro ao tentar mostrar usuário",
+    });
+    return;
+  }
+});
+
+// Criar a rota para EDITAR usuário
+router.put("/users/:id", async (req: Request, res: Response) => {
+  try {
+    // Obter ID do usuario
+    const { id } = req.params;
+
+    // res.send(`Editar: ${id}`); ------X---- IMPORTANTE ----X-----
+    // Receber os dados enviados no corpo da Requisição
+    const data = req.body;
+
+    // Obter o repositório da entidade User
+    const userRepository = AppDataSource.getRepository(User);
+
+    // Buscar no banco de dados pelo ID
+    const user = await userRepository.findOneBy({ id: parseInt(id) });
+
+    // Verificar se o usuário foi encontrado!
+
+    if (!user) {
+      res.status(404).json({
+        message: "Usuario não encontrado!",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      message: "Usuario atualizado com sucesso!",
+      user: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Erro ao editar usuário!",
+    });
+  }
+});
+
 // Criar a rota para cadastrar usuário
 router.post("/users", async (req: Request, res: Response) => {
   try {
